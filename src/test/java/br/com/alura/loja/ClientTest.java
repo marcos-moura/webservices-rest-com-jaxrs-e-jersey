7ae544f4921d7d1d5db8fa4943b685c38328d85c
manmo <manmo@LAPTOP-MVU12DJ8.hub>
2019-09-05 21:15:24 +1200

    Adding Client Test

---
+++ src/test/java/br/com/alura/loja/ClientTest.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class ClientTest {
+
+	@Test
+	public void testaQueAConexaoComOServidorFunciona() {
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://www.mocky.io");
+		String conteudo = target.path("/v2/52aaf5bbee7ba8c60329fb7b").request().get(String.class);
+		Assert.assertTrue(conteudo.contains("Rua"));
+	}
+}
